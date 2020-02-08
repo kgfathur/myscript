f1e471e00d9e29e88b7e0b151df2bac5dd454f5e
kgfathur <sahabatsolder@gmail.com>
2020-02-09 00:06:43 +0700

    add git-create

--- 00-aliases.sh
+++ 00-aliases.sh
@@ -7,4 +7,6 @@ alias gsaf='f(){ gnome-screenshot -a -f $1.png;  unset -f f; }; f'
 alias gsif='f(){ gnome-screenshot -i -f $1.png;  unset -f f; }; f'
 alias gssf='f(){ gnome-screenshot -f $1.png;  unset -f f; }; f'
 alias gswf='f(){ gnome-screenshot -w -f $1.png;  unset -f f; }; f'
+alias git-create='f(){ curl -u $1 https://api.github.com/user/repos -d "{\"name\":\"$2\"}";  unset -f f; }; f'
+
 
